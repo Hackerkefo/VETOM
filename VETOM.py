2b4933ad0c7a8230720a1a0611a33848ad10cf65
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-11 08:21:56 +0100

    .

--- VETOM.py
+++ VETOM.py
@@ -1,1 +1,16 @@
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(M))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("VETOM").lisensi()
+	else:
+		__import__("VETOM").lisensi()
 
